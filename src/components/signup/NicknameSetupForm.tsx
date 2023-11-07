9a4d3d23d239eb7e669e1df3b22ddb30c614a88e
uussong <77879633+uussong@users.noreply.github.com>
2023-11-07 22:29:17 +0900

    [#7] feat: 닉네임 유효성 검사 추가

--- src/components/signup/NicknameSetupForm.tsx
+++ src/components/signup/NicknameSetupForm.tsx
@@ -1,14 +1,36 @@
+import { useState } from 'react'
+
 interface NicknameSetupProps {
+  nickname: string
+  setNickname: React.Dispatch<React.SetStateAction<string>>
   title: string
   buttonText: string
   children?: React.ReactNode
 }
 
 function NicknameSetupForm({
+  nickname,
+  setNickname,
   title,
   buttonText,
   children,
 }: NicknameSetupProps) {
+  const [isNicknameValid, setIsNicknameValid] = useState(true)
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const regex = /^[ㄱ-ㅎ가-힣a-zA-Z0-9]*$/
+    const inputValue = e.target.value
+
+    if (regex.test(inputValue)) {
+      setNickname(inputValue)
+      setIsNicknameValid(true)
+    } else {
+      setIsNicknameValid(false)
+    }
+  }
+
+  const handleBlur = () => {
+    setIsNicknameValid(true)
+  }
   return (
     <>
       <form>
@@ -16,9 +38,15 @@ function NicknameSetupForm({
         <label>
           닉네임
           <input
+            value={nickname}
+            onChange={handleInput}
+            onBlur={handleBlur}
             type="text"
             placeholder="한글 영어 대/소문자 숫자만 입력 가능해요!"
           />
+          {!isNicknameValid && (
+            <span>한글 영어 대/소문자 숫자만 입력 가능해요!</span>
+          )}
         </label>
 
         <button type="submit">{buttonText}</button>
--- src/pages/AnonymousSignIn.tsx
+++ src/pages/AnonymousSignIn.tsx
@@ -1,8 +1,17 @@
+import { useState } from 'react'
+
 import NicknameSetupForm from '@/components/signup/NicknameSetupForm'
 
 function AnonymousSignInPage() {
+  const [nickname, setNickname] = useState('')
+
   return (
-    <NicknameSetupForm title="닉네임을 입력해주세요" buttonText="시작하기" />
+    <NicknameSetupForm
+      nickname={nickname}
+      setNickname={setNickname}
+      title="닉네임을 입력해주세요"
+      buttonText="시작하기"
+    />
   )
 }
 
