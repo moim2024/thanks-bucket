95f9aa3f895943be83142d32624d82b87f7cf819
limhizy15 <limhizy15@ajou.ac.kr>
2023-11-07 20:27:58 +0900

    [#6] feat: 라우터 구성

---
+++ src/components/shared/Layout.tsx
@@ -0,0 +1,22 @@
+import { Link, Outlet } from 'react-router-dom'
+
+function Layout() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to={'/buckets'}>마이 버킷</Link>
+          </li>
+          <li>
+            <Link to={'/feed'}>피드</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+}
+
+export default Layout
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '@/index.css'
-import App from '@/App'
 import reportWebVitals from '@/reportWebVitals'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import ErrorPage from './components/shared/RouteError'
+import Layout from './components/shared/Layout'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'buckets',
+        element: (
+          <div>
+            <span>Buckets</span>
+          </div>
+        ),
+      },
+      {
+        path: 'feed',
+        element: (
+          <div>
+            <span>Feed</span>
+          </div>
+        ),
+      },
+    ],
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
 
