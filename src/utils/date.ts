c5879c6e0e73e8fbfe6e484aa6967aa2847b2984
limhizy15 <limhizy15@ajou.ac.kr>
2023-11-09 23:02:25 +0900

    [#5] feat: 버킷 리스트 조회 기능 구현

---
+++ src/utils/date.ts
@@ -0,0 +1,8 @@
+/**
+ * Date 객체를 받아 YYYY.MM.DD 형태를 반환합니다.
+ * TODO: 회의를 통해 날짜는 앞으로 어떻게 포맷팅할 것인지 결정이 필요합니다.
+ * @param date
+ */
+export const formatDate = (date: Date) => {
+  return date.toLocaleDateString('ko-KR')
+}
