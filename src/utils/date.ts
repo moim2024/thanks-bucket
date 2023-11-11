ce9d98485f17d13e4f41d79233bced86b126ea42
limhizy15 <limhizy15@ajou.ac.kr>
2023-11-11 22:56:19 +0900

    [#5] refactor: date 유틸함수 date-fns 사용으로 변경

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,8 +1,10 @@
+import format from 'date-fns/format'
+
 /**
  * Date 객체를 받아 YYYY.MM.DD 형태를 반환합니다.
  * TODO: 회의를 통해 날짜는 앞으로 어떻게 포맷팅할 것인지 결정이 필요합니다.
  * @param date
  */
 export const formatDate = (date: Date) => {
-  return date.toLocaleDateString('ko-KR')
+  return format(date, 'yyyy.MM.dd')
 }
