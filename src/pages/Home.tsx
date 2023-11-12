cca7b064e89c839c36c4fd169b378e157868d1ac
limhizy15 <limhizy15@ajou.ac.kr>
2023-11-12 13:26:46 +0900

    [#4] 버킷 생성 기능 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,18 @@
 import BucketList from '@/components/home/BucketList'
+import { useNavigate } from 'react-router-dom'
 
 function HomePage() {
+  const navigate = useNavigate()
+
+  const handleCreateBucket = () => {
+    navigate('/bucket/create')
+  }
+
   return (
     <div>
       <h1>마이버킷</h1>
+
+      <button onClick={handleCreateBucket}>버킷 추가</button>
       <BucketList />
     </div>
   )
