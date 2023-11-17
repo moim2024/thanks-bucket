73e226d4bc4f60d955aa53b7b6087ccd355fd2b5
uussong <77879633+uussong@users.noreply.github.com>
2023-11-17 21:05:24 +0900

    [#7] refactor: handleSignOut 함수에 useCallback 추가

--- src/components/shared/SignOut.tsx
+++ src/components/shared/SignOut.tsx
@@ -2,11 +2,12 @@ import { signOut } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 
 import { auth } from '@/remote/firebase'
+import { useCallback } from 'react'
 
 function SignOut() {
   const navigate = useNavigate()
 
-  const handleSignOut = async () => {
+  const handleSignOut = useCallback(async () => {
     try {
       await signOut(auth)
 
@@ -14,7 +15,7 @@ function SignOut() {
     } catch (error) {
       console.error('로그아웃을 다시 시도해 주세요.', error)
     }
-  }
+  }, [navigate])
   return <button onClick={handleSignOut}>로그아웃</button>
 }
 
